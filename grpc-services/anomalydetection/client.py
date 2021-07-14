c57db62dace25ffe267d37ba2366e14e82e3bbbc
Robert Cullen <cinquaterra@gmail.com>
2021-07-14 07:28:05 -0400

    Added bidirectional streaming

--- grpc-services/anomalydetection/client.py
+++ grpc-services/anomalydetection/client.py
@@ -1,18 +1,31 @@
 import anomaly_detection_pb2_grpc as anomalies_service
 import detection_types_pb2 as anomalies_types
 from client_wrapper import ServiceClient
+from os import walk
 
+def make_request():
+    filenames = next(walk("files"), (None, None, []))[2]
+    for fn in filenames:
+        with open('files/' + fn, "r") as f:
+            data1 = f.read() + "\n"
+            request = anomalies_types.Request(
+                raw_json=data1, only_last='day'
+            )
+        return request
+
+def generate_requests():
+    requests = [
+        make_request()
+    ]
+
+    for req in requests:
+        yield req
 
 def run():
-    anomalies = ServiceClient(anomalies_service, 'AnomaliesStub', 'localhost', 50051)
+    anomalies = ServiceClient(anomalies_service, 'AnomalyDetectionStub', 'localhost', 50051)
     # Insert example metadata
     metadata = [('ip', '127.0.0.1')]
-    with open('test_data_1.csv', "r") as f:
-        data1 = f.read() + "\n"
-        request = anomalies_types.Request(
-            raw_json=data1, only_last='day'
-        )
-    response = anomalies.TwitterAnomalies(request)
+    response = anomalies.TwitterAnomalies(generate_requests())
     for resp in response:
         print(resp)
 
--- grpc-services/anomalydetection/server/server.py
+++ grpc-services/anomalydetection/server/server.py
@@ -16,17 +16,18 @@ class AnomalyDetectionService(anomalies_service.AnomalyDetectionServicer):
     def dparserfunc(self, date):
         return pd.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 
-    def TwitterAnomalies(self, request, context):
-        raw_csv = StringIO(request.raw_json)
-        only_last = request.only_last
-        data1 = pd.read_csv(raw_csv, index_col='timestamp', parse_dates=True, squeeze=True, date_parser=self.dparserfunc) 
-        result = anomaly_detect_ts(data1, alpha=0.05, direction="both", only_last=only_last, plot=False, longterm=True)
+    def TwitterAnomalies(self, request_iterator, context):
+        for msq in request_iterator:
+            raw_csv = StringIO(msq.raw_json)
+            only_last = msq.only_last
+            data1 = pd.read_csv(raw_csv, index_col='timestamp', parse_dates=True, squeeze=True, date_parser=self.dparserfunc) 
+            result = anomaly_detect_ts(data1, alpha=0.05, direction="both", only_last=only_last, plot=False, longterm=True)
 
-        for key in result["anoms"].to_dict():
-            keypair = anomalies_types.KeyValuePair(
-                timestamp=key.strftime('%Y-%m-%d %H:%M:%S'), value=result['anoms'][key]
-            )
-            yield anomalies_types.Response(keypair=keypair)
+            for key in result["anoms"].to_dict():
+                keypair = anomalies_types.KeyValuePair(
+                    timestamp=key.strftime('%Y-%m-%d %H:%M:%S'), value=result['anoms'][key]
+                )   
+                yield anomalies_types.Response(keypair=keypair)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
