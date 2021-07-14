d3900cc70e888761d3162f16091db6e5ad70e1a7
Robert Cullen <cinquaterra@gmail.com>
2021-07-14 13:38:14 -0400

    Updated client, added stream to client proto

--- grpc-services/anomalydetection/client.py
+++ grpc-services/anomalydetection/client.py
@@ -3,7 +3,8 @@ import detection_types_pb2 as anomalies_types
 from client_wrapper import ServiceClient
 from os import walk
 
-def make_request():
+def read_files():
+    file_list = []
     filenames = next(walk("files"), (None, None, []))[2]
     for fn in filenames:
         with open('files/' + fn, "r") as f:
@@ -11,22 +12,21 @@ def make_request():
             request = anomalies_types.Request(
                 raw_json=data1, only_last='day'
             )
-        return request
+        file_list.append(request)
+
+    return file_list
 
 def generate_requests():
-    requests = [
-        make_request()
-    ]
 
-    for req in requests:
+    for req in read_files():
         yield req
 
 def run():
     anomalies = ServiceClient(anomalies_service, 'AnomalyDetectionStub', 'localhost', 50051)
     # Insert example metadata
     metadata = [('ip', '127.0.0.1')]
-    response = anomalies.TwitterAnomalies(generate_requests())
-    for resp in response:
+    responses = anomalies.TwitterAnomalies(generate_requests())
+    for resp in responses:
         print(resp)
 
 
